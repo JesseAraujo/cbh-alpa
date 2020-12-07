85b274f502434266eba9a594c8f7f7b59711fc59
Jesse Brisola <jesse.araujo4@fatec.sp.gov.br>
2020-12-07 16:17:07 -0300

    update - new page

---
+++ src/views/Agenda/Reuniao-da-Conjunta-com-todas-as-Camaras-Tecnicas-07.12.2020.php
@@ -0,0 +1,107 @@
+<?php
+require '../../components/header.php';
+require '../../components/nav.php';
+?>
+
+
+
+<div class="content">
+
+    <!--
+--------------------------------------------------------
+BREADCRUMB
+--------------------------------------------------------
+-->
+    <section>
+        <div class="mapaSite">
+            <div class="container">
+                <ol class="breadcrumb mapaSite">
+                    <li class="breadcrumb-item"><a href="/">Início</a></li>
+                    <li class="breadcrumb-item"><a href="../Agenda-CBH-ALPA">Agenda</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"> Reunião da Conjunta com todas as Câmaras Técnicas</li>
+                </ol>
+            </div>
+        </div>
+    </section>
+
+
+    <!--
+--------------------------------------------------------
+TÍTULO DA PÁGINA
+--------------------------------------------------------
+-->
+    <section>
+        <div class="tituloPage">
+            <div class="container">
+                <label>Reunião da Conjunta com todas as Câmaras Técnicas</strong>
+            </div>
+        </div>
+    </section>
+
+
+    <!--
+--------------------------------------------------------
+CONTEUDO
+--------------------------------------------------------
+-->
+    <section>
+
+        <div class="container">
+
+            <p>Prezado (a) Senhor (a):</p>
+            <p>Cumprimentando Vossa Senhoria, servimo-nos do presente para convocá-lo (a) a participar da <strong>Reunião da Conjunta com todas as Câmaras Técnicas</strong>, conforme programação abaixo:</p>
+
+            <div class="not">
+                <p><span>Data:</span> 07 de dezembro de 2020 (segunda-feira)</p>
+                <p><span>Horário:</span> 10:00 às 11:30h</p>
+                <p><span>Local:</span> Videoconferência</p> <a href="https://teams.microsoft.com/l/meetup-join/19%3ameeting_NzhkYTliYmYtZDdkZC00OTZmLTlhMzAtZDUwNTBmOTM4N2Ey%40thread.v2/0?context=%7b%22Tid%22%3a%223a78b0cd-7c8e-4929-83d5-190a6cc01365%22%2c%22Oid%22%3a%22ecf38584-197e-4161-8528-5805416e5d72%22%7d">PARTICIPAR</a>
+            </div>
+
+            <p>Caso deseja participar como convidado, envie um e-mail para <a href="mailto:cbhalpa@gmail.com?Subject=47%20Reuniao%20Ordinaria%20-%20Convidado">cbhalpa@gmail.com</a></p>
+
+            <h3 style="margin-top:50px; margin-bottom:20px; font-size: 15px; background-color: #dddddd; text-align: center; padding:15px; font-family: LiberationSans-Regular;">
+                PAUTA DA REUNIÃO
+            </h3>
+
+            <ul class="item">
+                <li>Apresentação da Deliberação CBH-ALPA Ad Referendum nº 185, de 07 de dezembro
+                    de 2020, que define normas, procedimentos, critérios e institui a comissão eleitoral para
+                    o processo eleitoral dos membros, titulares e suplentes, do CBH-ALTO PARANAPANEMA
+                </li>
+            </ul>
+
+
+            <!-- DOCUMENTOS -->
+            <h3 style="margin-top:50px; margin-bottom:20px; font-size: 15px; background-color: #dddddd; text-align: center; padding:15px; font-family: LiberationSans-Regular;">
+                DOCUMENTOS
+            </h3>
+            <a class="icone" href="public/pdf/Oficios/2020/Of. 36 2020 CBH-ALPA - Reunião todas as Cts 07.12.2020.pdf" target="new">Convocação</a>
+
+
+
+            <!-- VIDEO DA GRAVAÇÃO DA REUNIÃO -->
+            <h3 style="margin-top:50px; margin-bottom:20px; font-size: 15px; background-color: #dddddd; text-align: center; padding:15px; font-family: LiberationSans-Regular;">
+                GRAVAÇÃO
+            </h3>
+
+            <video width="100%" controls>
+                <source src="src/assets/videos/Reuniao-da-Conjunta-com-todas-as-Camaras-Tecnicas-07.12.2020.mp4" type="video/mp4">
+                Your browser does not support HTML video.
+            </video>
+
+        </div>
+        <!--FIM CONTAINER-->
+    </section>
+
+</div>
+<!--content-->
+
+
+<!--
+--------------------------------------------------------
+FOOTER
+--------------------------------------------------------
+-->
+<?php
+require '../../components/footer.php';
+?>
